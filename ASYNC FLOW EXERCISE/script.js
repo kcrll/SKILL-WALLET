928453501a3aa5cbf4e85b4e62a3d42852738abc
kcrll <kiandg4@gmail.com>
2026-04-28 00:42:20 +0800

    Add files via upload

--- ASYNC FLOW EXERCISE/script.js
+++ ASYNC FLOW EXERCISE/script.js
@@ -1,7 +1,9 @@
+console.log("\n=== STEP 1: Synchronous Execution ===");
 console.log("A");
 console.log("B");
 console.log("C");
 
+console.log("\n=== STEP 2: Basic setTimeout ===");
 console.log("Start");
 
 setTimeout(() => {
@@ -10,6 +12,7 @@ setTimeout(() => {
 
 console.log("End");
 
+console.log("\n=== STEP 3: Basic Promise ===");
 console.log("Start");
 
 Promise.resolve().then(() => {
@@ -18,6 +21,7 @@ Promise.resolve().then(() => {
 
 console.log("End");
 
+console.log("\n=== STEP 4: Promise vs setTimeout ===");
 console.log("Start");
 
 setTimeout(() => {
@@ -30,6 +34,7 @@ Promise.resolve().then(() => {
 
 console.log("End");
 
+console.log("\n=== STEP 5: Async/Await ===");
 async function test() {
   console.log("1");
   await Promise.resolve();
@@ -40,6 +45,7 @@ console.log("3");
 test();
 console.log("4");
 
+console.log("\n=== STEP 6: Predict the Output ===");
 console.log("A");
 
 setTimeout(() => {
@@ -51,3 +57,23 @@ Promise.resolve().then(() => {
 });
 
 console.log("D");
+
+console.log("\n=== BONUS: Complex Event Loop ===");
+
+console.log("0");
+
+setTimeout(() => {
+  console.log("1");
+  Promise.resolve().then(() => console.log("1.1"));
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("2");
+  setTimeout(() => console.log("2.1"), 0);
+});
+
+setTimeout(() => {
+  console.log("3");
+}, 0);
+
+console.log("4");
